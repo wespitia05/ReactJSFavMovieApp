5d845b8aea3c766d766f7f4af0f670360ba5c8e2
william <espiw@farmingdale.edu>
2026-02-06 16:39:09 -0500

    added the jsx files we will be using, implemented a simple search bar connected to tmdb api and it returns results in the console

--- my-react-fav-movie-app/src/App.jsx
+++ my-react-fav-movie-app/src/App.jsx
@@ -1,6 +1,45 @@
+import { useState } from "react";
+import searchMulti from "./api/tmdb";
 
 function App() {
-    return(<></>);
+    // create state variable query (what user types)
+    // inital state is an empty string
+    // setQuery updates it and triggers a re-render
+    const [query, setQuery] = useState("");
+
+    // this function runs when the form is submitted
+    async function handleSubmit(event) {
+        // stops browser from refreshing the page when you submit the form
+        event.preventDefault();
+
+        // trim() removes leading/trailing whitespace
+        // if user submits empty text or spaces, exit early
+        if (!query.trim()) return;
+
+        // try/catch block so failed network requests wont crash the app
+        try {
+            // calls searchMulti function with cleaned query
+            // waits for it to finish, data is our json response from the tmdb
+            const data = await searchMulti(query.trim());
+            // print our results in the console
+            console.log("TMDb results:", data.results);
+        } 
+        // catches any error and prints it
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            placeholder="Search Movies or TV Shows..."
+            />
+            <button>Search</button>
+        </form>
+    );
 }
 
 export default App
---
+++ my-react-fav-movie-app/src/api/tmdb.js
@@ -0,0 +1,39 @@
+// safer way to import our api key from the .env file
+const api = import.meta.env.VITE_API_KEY;
+// base url for all tmdb api endpoints
+const base_url = "https://api.themoviedb.org/3";
+
+// async so we can use await and returns an object
+// our parameter is query which is what the user types
+async function searchMulti(query) {
+    // build our full request url
+    // ${base_url}/search/multi is the endpoint that searches across movies, tv and people
+    // ? starts the query parameters
+    // encodeURIComponent(quer) makes the search safe for urls
+    // include_adult=false filters out adult content
+    // language=en-US returns english text
+    // page=1 requests the first page of results
+    const url =
+    `${base_url}/search/multi?` +
+    `query=${encodeURIComponent(query)}` +
+    `&include_adult=false&language=en-US&page=1&api_key=${api}`;
+
+    // res is our response object, sends an http request to the tmdb
+    // await pauses until the response comes back
+    const res = await fetch(url);
+
+    // res.ok is true for status codes 200-299
+    // if res.ok is not true...
+    if (!res.ok) {
+        // if tmdb returns 401, 404, etc, we throw an error
+        throw new Error("TMDb search failed");
+    }
+
+    // converts the response body into a json, data becomes the parsed json
+    const data = await res.json();
+    // return the parsed json to whoever calls searchMulti
+    return data;
+}
+
+// exports function for other files to import
+export default searchMulti
