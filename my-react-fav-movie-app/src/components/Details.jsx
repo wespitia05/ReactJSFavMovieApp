d4c2f6ff8c5ad0c3b7f2fbb86f56dc9da08136b3
william <espiw@farmingdale.edu>
2026-03-23 22:27:35 -0400

    we can now hover over each component in studios, countries and languages (will add click attributes later)

--- my-react-fav-movie-app/src/components/Details.jsx
+++ my-react-fav-movie-app/src/components/Details.jsx
@@ -9,19 +9,46 @@ function Details({studios = [], countries = [], languages = []}) {
             <div className="details-row">
                 <span className="details-label">Studios: </span>
                 <span className="details-value">
-                    {studios.length > 0 ? studios.join(", ") : "Unknown"}
+                    {studios.length > 0 ? (
+                        studios.map((studio, index) => (
+                        <span key={studio} className="details-item">
+                            {studio}
+                            {index < studios.length - 1 && ", "}
+                        </span>
+                        ))
+                    ) : (
+                        "Unknown"
+                    )}
                 </span>
             </div>
             <div className="details-row">
                 <span className="details-label">Countries: </span>
                 <span className="details-value">
-                    {countries.length > 0 ? countries.join(", ") : "Unknown"}
+                    {countries.length > 0 ? (
+                        countries.map((country, index) => (
+                        <span key={country} className="details-item">
+                            {country}
+                            {index < countries.length - 1 && ", "}
+                        </span>
+                        ))
+                    ) : (
+                        "Unknown"
+                    )}
                 </span>
             </div>
             <div className="details-row">
                 <span className="details-label">Spoken Languages: </span>
                 <span className="details-value">
-                    {languages.length > 0 ? languages.join(", ") : "Unknown"}
+                    {languages.length > 0 ? (
+                        languages.map((language, index) => (
+                        <span key={language} className="details-item">
+                            {language}
+                            {index < languages.length - 1 && ", "}
+                        </span>
+                        ))
+                    ) : (
+                        "Unknown"
+                    )}
                 </span>
             </div>
         </div>
